b6ad4ffd19d0e7077df25bdc321938eb511b3c6b
Kim Eun Yeong <iris578@naver.com>
2023-07-04 02:32:34 +0900

    initial

---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
+
+export default function Nav() {
+    const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        });
+
+        return () => {
+            window.removeEventListener("scroll", () => { });
+        };
+    }, []);
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`)
+    }
+
+
+    return <nav className={`nav ${show && "nav__black"}`}>
+        <img
+            alt='Netflix logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png'
+            className='nav__logo'
+            onClick={() => window.location.reload()}
+        />
+        <input value={searchValue} onChange={handleChange} className='nav__input' type="text" placeholder='영화를 검색해주세요.' />
+        <img
+            alt="User logged"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            className='nav__avatar'
+        />
+    </nav>;
+}
